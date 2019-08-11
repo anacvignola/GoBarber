ecd7c7235a7fd648ea22766100615f8c22362287
anacvignola <anacvignola@gmail.com>
2019-08-11 12:01:59 -0300

    send emails with nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2f5cce0bfff84d',
+    pass: 'b59937b6c56b63',
+  },
+  default: {
+    from: 'Contact GoBarber <noreply@gobarber.com>',
+  },
+};
